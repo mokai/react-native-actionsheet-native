2a8927154ec3bc23ecffa010cd5fb56e022d8530
gongkai <865425695@qq.com>
2019-11-04 17:37:28 +0800

    support androidx

--- android/src/main/java/com/slowpath/actionsheet/ActionSheetPackage.java
+++ android/src/main/java/com/slowpath/actionsheet/ActionSheetPackage.java
@@ -1,16 +1,12 @@
 package com.slowpath.actionsheet;
 
-import android.support.v4.app.FragmentActivity;
-
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ActionSheetPackage implements ReactPackage {
